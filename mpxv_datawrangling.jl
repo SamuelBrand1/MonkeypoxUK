dca0e796953c1f4927bad5177901806963c4ab95
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2022-08-04 16:28:15 +0100

    update

--- mpxv_datawrangling.jl
+++ mpxv_datawrangling.jl
@@ -12,7 +12,7 @@ idxs_confirmed = UK_mpxv_data.Status .== "confirmed"
 # p/(1-p) = R ⟹ p = R(1-p) ⟹ p = R/(1+R)
 reported_msm_prop = [Date(2022,6,22) 0.96
                         Date(2022,7,6) 0.962;
-                        Date(2022,7,19) 0.965]
+                        Date(2022,7,19) 0.965] #https://www.gov.uk/government/publications/monkeypox-outbreak-technical-briefings/investigation-into-monkeypox-outbreak-in-england-technical-briefing-4
 
 msm_ratio = 1.0 ./ [Inf, 0.00000000, 0.01351351, 0.02222222, 0.05769231, 0.04575163,
         0.01408451, 0.03296703, 0.03125000, 0.07575758, 0.06172840, 0.09677419] # Ratio estimates
