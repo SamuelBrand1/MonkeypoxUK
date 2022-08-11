d4325aec2062958fcfbbe095694646106beaf56a
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2022-08-11 13:18:43 +0100

    Revert "Create posterior_param_draws_2022-08-08_ukhsa.jld2"
    This reverts commit 88bd0be94b7d46d562e94ffb2f5af24fa6af7c9a.

--- mpx_scenarios.jl
+++ mpx_scenarios.jl
@@ -10,16 +10,16 @@ include("setup_model.jl");
 
 ##Load posterior draws
 
-draws = load("posteriors/posterior_param_draws_2022-06-27.jld2")["param_draws"]
+param_draws = load("posteriors/posterior_param_draws_2022-06-27.jld2")["param_draws"]
 # smc = MonkeypoxUK.load_smc("posteriors/smc_posterior_draws_2022-08-01.jld2")
-##
-predictions = MonkeypoxUK.generate_scenario_projections(draws, wks, mpxv_wkly, constants)
+# ##
+# predictions = MonkeypoxUK.generate_scenario_projections(draws, wks, mpxv_wkly, constants)
 
-##
-plt = MonkeypoxUK.plot_case_projections(predictions, wks, mpxv_wkly; savefigure=true)
+# ##
+# plt = MonkeypoxUK.plot_case_projections(predictions, wks, mpxv_wkly; savefigure=true)
 
-##
-param_draws = [part.params for part in smc.particles]
+# ##
+# param_draws = [part.params for part in smc.particles]
 
 ## Public health emergency effect forecasts
 long_wks = [wks; [wks[end] + Day(7 * k) for k = 1:12]]
