9aa57b30aeb39bf0252606d26c654e920cbba39a
Samuel Brand <sam055@mac.com>
2022-09-16 09:55:59 +0100

    update vaccination rate

--- mpx_inference_parallel.jl
+++ mpx_inference_parallel.jl
@@ -74,10 +74,16 @@ setup_cng_pnt = ABCSMC(MonkeypoxUK.mpx_sim_function_chp, #simulation function
     constants=constants,
     maxiterations=10^6)
 
-    return runabc(setup_cng_pnt, mpxv_wkly, verbose=true, progress=false)
+    return runabc(setup_cng_pnt, mpxv_wkly, verbose=true, progress=true)
 end
 
 smcs = pmap(run_smc_with_target,fill(ϵ_target,2))
+
+##
+@save "posteriors/smcs.jld2" smcs
+param_draws = [[particle.params for particle in smcs[1].particles];[particle.params for particle in smcs[2].particles]]
+@save("posteriors/posterior_param_draws_"*string(wks[end])*".jld2", param_draws)
+
 
 ##Run ABC    
 smc_cng_pnt = runabc(setup_cng_pnt, mpxv_wkly, verbose=true, progress=false)
@@ -88,12 +94,12 @@ param_draws = [particle.params for particle in smc_cng_pnt.particles]
 ##posterior predictive checking - simple plot to see coherence of model with data
 
 
-post_preds = [part.other for part in smc_cng_pnt.particles]
+post_preds = [part.other for part in smcs[2].particles]
 plt = plot(; ylabel="Weekly cases",
     title="Posterior predictive checking")
 for pred in post_preds
 
-    plot!(plt, wks_inff, pred, lab="", color=[1 2], alpha=0.3)
+    plot!(plt, wks_inff, pred, lab="", color=[1 2], alpha=0.1)
 end
 scatter!(plt, wks_inff, mpxv_wkly_inff, lab=["Data: (MSM)" "Data: (non-MSM)"],ylims = (0,800))
 display(plt)
