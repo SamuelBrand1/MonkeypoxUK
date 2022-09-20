f67da6b1c093502bf8810823c92816e0581d9ac7
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2022-09-20 11:18:15 +0100

    fits

--- mpx_inference.jl
+++ mpx_inference.jl
@@ -29,7 +29,7 @@ prior_vect_cng_pnt = [Gamma(1, 1), # α_choose 1
 
 
 ## Use SBC for defining the ABC error target and generate prior predictive plots
-ϵ_target, plt_prc, hist_err = MonkeypoxUK.simulation_based_calibration(prior_vect_cng_pnt, wks, mpxv_wkly, constants; target_perc=0.25)
+ϵ_target, plt_prc, hist_err = MonkeypoxUK.simulation_based_calibration(prior_vect_cng_pnt, wks, mpxv_wkly, constants; target_perc=0.05)
 
 setup_cng_pnt = ABCSMC(MonkeypoxUK.mpx_sim_function_chp, #simulation function
     12, # number of parameters
@@ -37,7 +37,7 @@ setup_cng_pnt = ABCSMC(MonkeypoxUK.mpx_sim_function_chp, #simulation function
     Prior(prior_vect_cng_pnt); #Prior for each of the parameters
     ϵ1=1000,
     convergence=0.05,
-    nparticles=1000,
+    nparticles=2000,
     α=0.3,
     kernel=gaussiankernel,
     constants=constants,
@@ -45,7 +45,7 @@ setup_cng_pnt = ABCSMC(MonkeypoxUK.mpx_sim_function_chp, #simulation function
 
 ##Run ABC    
 smc_cng_pnt = runabc(setup_cng_pnt, mpxv_wkly, verbose=true, progress=true)
-@save("posteriors/smc_posterior_draws_"*string(wks[end])*"_vs2.jld2", smc_cng_pnt)
+@save("posteriors/smc_posterior_draws_"*string(wks[end])*".jld2", smc_cng_pnt)
 param_draws = [particle.params for particle in smc_cng_pnt.particles]
 @save("posteriors/posterior_param_draws_"*string(wks[end])*".jld2", param_draws)
 
