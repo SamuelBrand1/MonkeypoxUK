2308a861e19448b6f59c0d43f2b702c8c0fefa0d
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2022-10-06 10:11:53 +0100

    posterior plots

--- posterior_plots.jl
+++ posterior_plots.jl
@@ -52,139 +52,37 @@ chn = Chains(val_mat, param_names)
 CSV.write("posteriors/posterior_chain_" * string(wks[end]) * ".csv", DataFrame(chn))
 
 ##Calculate orignal R₀ and latest R(t)
-function construct_next_gen_mat(params, constants, susceptible_prop, vac_rates)
-    
+"""
+function construct_next_gen_mat(params, constants, susceptible_prop, vac_rates; vac_eff::Union{Nothing,Number} = nothing)
+
+Construct the next generation matrix `G` in orientation: 
+> `G_ij = E[# Infected people in group j due to one in group i]`
+Returns `(Real(eigvals(G)[end]), G)`. NB: `Real(eigvals(G)[end])` is the leading eignvalue of `G` i.e. the reproductive number.
+"""
+function construct_next_gen_mat(params, constants, susceptible_prop, vac_rates; vac_eff::Union{Nothing,Number} = nothing)
     #Get parameters 
     α_choose, p_detect, mean_inf_period, p_trans, R0_other, M, init_scale, chp_t, trans_red, trans_red_other, scale_trans_red2, scale_red_other2 = params
     #Get constant data
-    N_total, N_msm, ps, ms, ingroup, ts, α_incubation, n_cliques, wkly_vaccinations, vac_effectiveness, chp_t2 = constants
+    N_total, N_msm, ps, ms, ingroup, ts, α_incubation, n_cliques, wkly_vaccinations, vac_effectiveness, chp_t2 = copy(constants)
 
-    #Calculate next gen matrix
+    if ~isnothing(vac_eff)
+        vac_effectiveness = vac_eff
+    end
+
+    #Calculate next gen matrix G_ij = E[#Infections in group j due to a single infected person in group i]
     _A = (ms .* (susceptible_prop .+  (vac_rates .* (1.0 .- vac_effectiveness)))') .* (mean_inf_period .* p_trans ./ length(ms) )  #Sexual transmission within MSM
     A = _A .+ (R0_other/N_uk) .* repeat(ps' .* N_msm,10) #Other routes of transmission MSM -> MSM
-    B = (R0_other*(N_uk - N_msm)/N_total) .* ones(10)    # MSM transmission to non MSM
+    B = (R0_other*(N_uk - N_msm)/N_total) .* ones(10) # MSM transmission to non MSM
     C = (R0_other/N_uk) .* ps' .* N_msm  #Non-msm transmission to MSM
     D = [ (R0_other*(N_uk - N_msm)/N_total) ]# Non-MSM transmission to non-MSM
     G = [A B;C D]
     return Real(eigvals(G)[end]), G
 end
 
-function generate_Rt_estimate(params, constants, wkly_cases)
-    #Get constant data
-    N_total, N_msm, ps, ms, ingroup, ts, α_incubation, n_cliques, wkly_vaccinations, vac_effectiveness, chp_t2 = constants
-
-    #Get parameters and make transformations
-    α_choose, p_detect, mean_inf_period, p_trans, R0_other, M, init_scale, chp_t, trans_red, trans_red_other, scale_trans_red2, scale_red_other2 = params
-    p_γ = 1 / (1 + mean_inf_period)
-    γ_eff = -log(1 - p_γ) #get recovery rate
-    trans_red2 = trans_red * scale_trans_red2
-    trans_red_other2 = scale_trans_red2 * scale_red_other2
-
-    #Generate random population structure
-    u0_msm, u0_other, N_clique, N_grp_msm = MonkeypoxUK.setup_initial_state(N_total, N_msm, α_choose, p_detect, α_incubation, ps, init_scale; n_states=9, n_cliques=n_cliques)
-    Λ, B = MonkeypoxUK.setup_transmission_matrix(ms, ps, N_clique; ingroup=ingroup)
-
-    #Simulate and track error
-    L1_rel_err = 0.0
-    total_cases = sum(wkly_cases[1:(end-1), :])
-    u_mpx = ArrayPartition(u0_msm, u0_other)
-    prob = DiscreteProblem((du, u, p, t) -> MonkeypoxUK.f_mpx_vac(du, u, p, t, Λ, B, N_msm, N_grp_msm, N_total),
-        u_mpx, (ts[1] - 7, ts[1] - 7 + 7 * size(wkly_cases, 1)),#lag for week before detection
-        [p_trans, R0_other, γ_eff, α_incubation, vac_effectiveness])
-    mpx_init = init(prob, FunctionMap(), save_everystep=false) #Begins week 1
-    old_onsets = [0, 0]
-    new_onsets = [0, 0]
-    wk_num = 1
-    detected_cases = zeros(size(wkly_cases))
-    not_changed = true
-    not_changed2 = true
-
-    p_trans_now = mpx_init.p[1]
-    R0_other_now = mpx_init.p[2]
-
-    # Get initial state
-    u = deepcopy(mpx_init.u)
-    S = u.x[1][1, :, :]
-    I = u.x[1][6, :, :]
-    V = u.x[1][8, :, :]
-    S_other = u.x[2][1]
-    I_other = u.x[2][6]
-
-    # Transmission rate now instantaneous R(t)
-    total_I = I_other + sum(I)
-    λ = (p_trans_now .* (Λ * I * B)) ./ (N_grp_msm .+ 1e-5)
-    λ_other = γ_eff * R0_other_now * total_I / N_total
-
-    total_inf_rate = sum((S .* (1 .- exp.(-(λ .+ λ_other)))) .+ (V .* (1 .- exp.(-(1 - vac_effectiveness) * (λ .+ λ_other))))) 
-    total_inf_rate += S_other * (1 - exp(-λ_other))
-    R_0 = total_I > 0 ? mean_inf_period * total_inf_rate / total_I : 0.0
-
-
-
-    while wk_num <= size(wkly_cases, 1)
-        if not_changed && mpx_init.t > chp_t ##Change point for transmission
-            not_changed = false
-            mpx_init.p[1] = mpx_init.p[1] * (1 - trans_red) #Reduce transmission per sexual contact after the change point
-            mpx_init.p[2] = mpx_init.p[2] * (1 - trans_red_other) #Reduce transmission per non-sexual contact after the change point
-        end
-        if not_changed2 && mpx_init.t > chp_t2 ##2nd change point for transmission 
-            not_changed2 = false
-            mpx_init.p[1] = mpx_init.p[1] * (1 - trans_red2) #Reduce sexual MSM transmission after the change point
-            mpx_init.p[2] = mpx_init.p[2] * (1 - trans_red_other2) #Reduce  other transmission after the change point
-        end
-        step!(mpx_init, 7)#Step forward a week
-
-        #Do vaccine uptake
-        nv = wkly_vaccinations[wk_num]#Mean number of vaccines deployed
-        du_vac = deepcopy(mpx_init.u)
-        vac_rate = nv .* du_vac.x[1][1, 3:end, :] / (sum(du_vac.x[1][1, 3:end, :]) .+ 1e-5)
-        num_vaccines = map((μ, maxval) -> min(rand(Poisson(μ)), maxval), vac_rate, du_vac.x[1][1, 3:end, :])
-        du_vac.x[1][1, 3:end, :] .-= num_vaccines
-        du_vac.x[1][8, 3:end, :] .+= num_vaccines
-        set_u!(mpx_init, du_vac) #Change the state of the model
-
-        #Calculate actual onsets, generate observed cases and score errors        
-        new_onsets = [sum(mpx_init.u.x[1][end, :, :]), mpx_init.u.x[2][end]]
-        actual_obs = [rand(BetaBinomial(new_onsets[1] - old_onsets[1], p_detect * M, (1 - p_detect) * M)), rand(BetaBinomial(new_onsets[2] - old_onsets[2], p_detect * M, (1 - p_detect) * M))]
-        detected_cases[wk_num, :] .= actual_obs #lag 1 week
-
-        wk_num += 1
-        old_onsets = new_onsets
-    end
-    
-    #Calculate final R(t)
-    p_trans_now = mpx_init.p[1]
-    R0_other_now = mpx_init.p[2]
-
-    # Get final state
-    u = deepcopy(mpx_init.u)
-    S = u.x[1][1, :, :]
-    I = u.x[1][6, :, :]
-    V = u.x[1][8, :, :]
-    S_other = u.x[2][1]
-    I_other = u.x[2][6]
-
-    # Transmission rate now instantaneous R(t)
-    total_I = I_other + sum(I)
-    λ = (p_trans_now .* (Λ * I * B)) ./ (N_grp_msm .+ 1e-5)
-    λ_other = γ_eff * R0_other_now * total_I / N_total
-
-    total_inf_rate = sum((S .* (1 .- exp.(-(λ .+ λ_other)))) .+ (V .* (1 .- exp.(-(1 - vac_effectiveness) * (λ .+ λ_other))))) 
-    total_inf_rate += S_other * (1 - exp(-λ_other))
-    R_t = total_I > 0 ? mean_inf_period * total_inf_rate / total_I : 0.0
-
-    return R_t, R_0, S, I, p_trans_now, R0_other_now
-end
-
 ## Calculate the original R0 with next gen matrix method and lastest R(t)
 R0s = map(θ -> construct_next_gen_mat(θ,constants, [ones(10); zeros(0)], [zeros(10);fill(1.0,0)])[1],param_draws )
-initial_Rts = map(θ -> generate_Rt_estimate(θ, constants, mpxv_wkly)[2], param_draws)
-
-latest_Rts = map(θ -> generate_Rt_estimate(θ, constants, mpxv_wkly)[1], param_draws)
 
 @show round(mean(R0s),digits = 2),round.(quantile(R0s,[0.1,0.9]),digits = 2)
-@show round(mean(latest_Rts), digits = 2), round.(quantile(latest_Rts,[0.1,0.9]), digits = 2)
-@show round(mean(initial_Rts), digits = 2), round.(quantile(initial_Rts[initial_Rts .> 1.0],[0.1,0.9]), digits = 2)
 
 
 
