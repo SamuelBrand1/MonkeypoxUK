9bcb590b0b160a096c7a957481e3f6a38cc3ddf4
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2022-11-03 15:52:40 +0000

    removed or moved old data sources/formats

--- mpxv_datawrangling_inff.jl
+++ mpxv_datawrangling_inff.jl
@@ -2,15 +2,6 @@ using CSV, DataFrames, Plots, Dates, Plots.PlotMeasures
 
 ## MSM data with data inference
 
-mpxv_data_inferred = CSV.File("data/mxpv_wkly_inferredmsm.csv") |> DataFrame
-wks = Date.(mpxv_data_inferred.week, DateFormat("dd/mm/yyyy"))
-mpxv_wkly = [mpxv_data_inferred.gbmsm mpxv_data_inferred.nongbmsm]
-
-##
-
-past_mpxv_data_inferred = CSV.File("data/weekly_data_imputation_2022-09-30.csv",
+past_mpxv_data_inferred = CSV.File("data/weekly_data_imputation_2022-10-26.csv",
                                 missingstring = "NA") |> DataFrame
 
-past_mpxv_data_inferred_latest = CSV.File("data/weekly_data_imputation_2022-10-06.csv",
-                                missingstring = "NA") |> DataFrame
-                                
