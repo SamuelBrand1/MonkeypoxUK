392af73d56d45a3d2ed113673fccb1882aba5907
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2023-01-18 14:58:39 +0000

    script tidy up

--- mpxv_datawrangling_inff.jl
+++
@@ -1,8 +0,0 @@
-using CSV, DataFrames, Plots, Dates, Plots.PlotMeasures
-
-## MSM data with data inference
-
-past_mpxv_data_inferred =
-    CSV.File("data/weekly_data_imputation_2022-9-15.csv", missingstring = "NA") |> DataFrame
-
-wks = Date.(past_mpxv_data_inferred.week, DateFormat("dd/mm/yyyy"))
